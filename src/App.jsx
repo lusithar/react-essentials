8cf20b5d97bb25136f347efebb8d0d6f429cfefe
lusithar <lusithar@ccl.lk>
2025-06-08 07:50:43 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <li><TabButton>Component</TabButton></li>
+            <li><TabButton>JSX</TabButton></li>
+            <li><TabButton>Props</TabButton></li>
+            <li><TabButton>State</TabButton></li>
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,10 @@
+export default function TabButton(props) {
+    function handleClick() {
+        console.log("Clicked");
+    }
+  return (
+    <li>
+      <button onClick={handleClick}>{props.children}</button>
+    </li>
+  );
+}
